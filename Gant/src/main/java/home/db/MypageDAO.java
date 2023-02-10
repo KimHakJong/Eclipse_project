841264fef436e1ee1541d14c6c619441d484f454
czzee <dlqtk0825@gmail.com>
2023-02-10 10:23:29 +0900

    최준호33333333

--- Gant/src/main/java/home/db/MypageDAO.java
+++ Gant/src/main/java/home/db/MypageDAO.java
@@ -12,11 +12,11 @@ import javax.sql.DataSource;
 import net.db.Members;
 
 public class MypageDAO {
-	
+
 	private DataSource ds;
-	
+
 	public MypageDAO() {
-		
+
 		try {
 			Context init = new InitialContext();
 			ds = (DataSource) init.lookup("java:comp/env/jdbc/OracleDB");
@@ -25,8 +25,8 @@ public class MypageDAO {
 			return;
 		}
 	}
-	
-	
+
+
 	// 자기에 대한 정보 가져오기
 	public Members Myinfo (String id) {
 		Connection conn = null;
@@ -35,16 +35,17 @@ public class MypageDAO {
 		Members m = null;
 		try {
 			conn = ds.getConnection();
-			
+
 			String sql = "select name, id, password, phone_num, "
-						+ " email, post, address, department, position, jumin "
-						+ " from members "
-						+ " where id = ? ";
-			
+					+ " email, post, address, department, position, jumin, "
+					+ " profileimg "
+					+ " from members "
+					+ " where id = ? ";
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-			
+
 			if(rs.next()) {
 				m = new Members();
 				m.setName(rs.getString("name"));
@@ -57,9 +58,10 @@ public class MypageDAO {
 				m.setDepartment(rs.getString("department"));
 				m.setPosition(rs.getString("position"));
 				m.setJumin(rs.getString("jumin"));
+				m.setProfileimg(rs.getString("profileimg"));
 			}
-			
-			
+
+
 		} catch(Exception e) {
 			e.printStackTrace();
 			System.out.println("Myinfo()에러 발생");
@@ -88,5 +90,58 @@ public class MypageDAO {
 		}
 		return m;
 	}
-	
+
+
+	// 개인정보 수정
+	public int update(Members m) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			conn = ds.getConnection();
+			String sql = "update members "
+					  + " set password = ?, phone_num = ?, email = ?, "
+					  + " post = ?, "
+					  + " address = ?, department = ?, "
+					  + " position = ?, profileimg = ? "
+					  + " where id = ? ";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, m.getPassword());
+			pstmt.setString(2, m.getPhone_num());
+			pstmt.setString(3, m.getEmail());
+			pstmt.setInt(4, m.getPost());
+			pstmt.setString(5, m.getAddress());
+			pstmt.setString(6, m.getDepartment());
+			pstmt.setString(7, m.getPosition());
+			pstmt.setString(8, m.getProfileimg());
+			pstmt.setString(9, m.getId());
+			
+			result = pstmt.executeUpdate();
+			if(result == 0) {
+				System.out.println("개인정보 변경하지 못했습니다");
+			}
+			
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("update()에러 : 개인정보 수정 부분 에러");
+		} finally {
+			try {
+				if(pstmt!=null)
+					pstmt.close();
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+			try {
+				if(conn!=null)
+					conn.close();
+			} catch(SQLException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 }
