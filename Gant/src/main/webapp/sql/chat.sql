11d51bc854ee1509e7c496ce07b69f31d9ce804e
김학종 <@DESKTOP-97QKNCN>
2023-02-13 09:27:34 +0900

    commit1

--- Gant/src/main/webapp/sql/chat.sql
+++
@@ -1,7 +0,0 @@
-create table chat(
-id		 	varchar2(15) references members(id) on delete cascade,
-name 		varchar2(15), 
-contents	varchar2(2000), 
-regdate 	date default sysdate, 
-chatimg 	varchar2(50)
-);
