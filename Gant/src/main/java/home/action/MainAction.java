d9eea03efddbb115743bc87e041bd633ba93b0eb
czzee <dlqtk0825@gmail.com>
2023-02-13 12:40:24 +0900

    최준호

--- Gant/src/main/java/home/action/MainAction.java
+++ Gant/src/main/java/home/action/MainAction.java
@@ -7,10 +7,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import net.db.MembersDAO;
+
 
 public class MainAction implements Action{
 	public ActionForward execute(HttpServletRequest request, 
 			HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		String id = (String) session.getAttribute("id");
+		
+		MembersDAO mdao = new MembersDAO();
+		mdao.getProfileimg(id);
+		session.setAttribute("profileimg", mdao.getProfileimg(id));
+		
 		
 		ActionForward forward = new ActionForward();
 		forward.setRedirect(false);
